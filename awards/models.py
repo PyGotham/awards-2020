8ac19b09ca2b68110e0561761b740e8b1b3ede87
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-03-20 01:27:57 -0400

    Add user roles
    While we don't have any immediate for roles yet, they're a requirement
    of [Flask-Security](https://pypi.org/p/flask-security)'s datastores.
    We'll eventually need roles for things like admins and approvers so this
    is just giving us a head start.

--- awards/models.py
+++ awards/models.py
@@ -1,10 +1,24 @@
 from datetime import datetime
 
 from citext import CIText  # type: ignore[import]
-from sqlalchemy import Column, Integer
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import backref, relationship
+from sqlalchemy.schema import UniqueConstraint
 from sqlalchemy.types import TIMESTAMP
 
 from .db import Base
+
+
+class Role(Base):
+    __tablename__ = "roles"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(CIText, nullable=False, unique=True)
+    description = Column(String(255), nullable=False, default="")
+    created = Column(TIMESTAMP, nullable=False, default=datetime.utcnow)
+    modified = Column(
+        TIMESTAMP, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
 
 
 class User(Base):
@@ -16,3 +30,19 @@ class User(Base):
     modified = Column(
         TIMESTAMP, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow
     )
+
+    roles = relationship(
+        "Role",
+        secondary="user_roles",
+        backref=backref("users", lazy="dynamic"),  # type: ignore[no-untyped-call]
+    )
+
+
+class UserRole(Base):
+    __tablename__ = "user_roles"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    role_id = Column(Integer, ForeignKey("roles.id"))
+
+    __table_args__ = (UniqueConstraint("user_id", "role_id"),)
