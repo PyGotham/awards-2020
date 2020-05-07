b3b69f73c76474e5fef8ff21909e7f301a17dc93
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-05-07 01:27:56 -0400

    Add financial aid application fields
    This will add fields to the financial aid application to gather
    information about travel reimbursement and lodging needs. We've
    historically asked for information about room sharing, but I'm going to
    hold off on adding that for right now.

---
+++ src/applications/test_forms.py
@@ -0,0 +1,47 @@
+from applications.forms import FinancialAidApplicationForm
+
+
+def test_financial_aid_lodging_requested_is_treated_as_boolean() -> None:
+    other_fields = {"background": "a", "reason_to_attend": "b"}
+
+    form = FinancialAidApplicationForm(other_fields)
+    assert form.is_valid()
+    # pyre-ignore[16]: This is fixed by https://github.com/facebook/pyre-check/pull/261.
+    assert form.cleaned_data["lodging_requested"] is False
+
+
+def test_financial_aid_travel_amount_must_be_greater_than_zero() -> None:
+    other_fields = {"background": "a", "reason_to_attend": "b"}
+
+    form = FinancialAidApplicationForm(
+        {"travel_requested": True, "travel_amount": -1, **other_fields}
+    )
+    assert not form.is_valid()
+    assert "Your estimated travel costs cannot be negative." in form.errors["__all__"]
+
+    form = FinancialAidApplicationForm(
+        {"travel_requested": True, "travel_amount": 0, **other_fields}
+    )
+    assert not form.is_valid()
+    assert (
+        "Your estimated travel costs must be greater than $0.00."
+        in form.errors["__all__"]
+    )
+
+    form = FinancialAidApplicationForm(
+        {"travel_requested": True, "travel_amount": 1, **other_fields}
+    )
+    assert form.is_valid()
+
+
+def test_financial_aid_travel_must_be_requested_if_amount_specified() -> None:
+    other_fields = {"background": "a", "reason_to_attend": "b"}
+
+    form = FinancialAidApplicationForm(
+        {"travel_requested": False, "travel_amount": 1, **other_fields}
+    )
+    assert not form.is_valid()
+    assert (
+        "You must request travel assistance before providing an estimated cost."
+        in form.errors["__all__"]
+    )
