55738ca7704a4bd01e3fdfb8c46a6ae2ef9d75a1
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-05-06 23:35:37 -0400

    Track application type
    This adds a new `Application.type` field that can be used to track which
    type of request the application is for. Since it already exists, the
    default type will be scholarship. It will be assigned to all existing
    applications in the database via a data migration.

--- src/applications/models.py
+++ src/applications/models.py
@@ -13,6 +13,11 @@ class Application(models.Model):
     class Status(models.TextChoices):
         PENDING = "pending"
 
+    # pyre-ignore[11]: This is fixed by https://github.com/facebook/pyre-check/pull/256.
+    class Type(models.TextChoices):
+        FINANCIAL_AID = "finaid"
+        SCHOLARSHIP = "scholarship"
+
     # pyre-ignore[16]: This is fixed by https://github.com/facebook/pyre-check/pull/256.
     applicant = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     background = models.TextField(_("applicant background"))
@@ -23,6 +28,18 @@ class Application(models.Model):
         choices=Status.choices,
         default=Status.PENDING,
     )
+    type = models.CharField(
+        max_length=11,
+        # pyre-ignore[16]: This is fixed by https://github.com/facebook/pyre-check/pull/256.
+        choices=Type.choices,
+        default=Type.SCHOLARSHIP,
+    )
 
     def __str__(self) -> str:
-        return f"Application for {self.applicant}"
+        # TODO: Once these ignores are removed, type_ and type_name
+        # should be combined into a single expression.
+        # pyre-ignore[19]: This is fixed by https://github.com/facebook/pyre-check/pull/256.
+        type_ = Application.Type(self.type)
+        # pyre-ignore[16]: This is fixed by https://github.com/facebook/pyre-check/pull/256.
+        type_name = type_.name.replace("_", " ").title()
+        return f"{type_name} application for {self.applicant}"
