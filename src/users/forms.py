a8bada402ced7ceff00cf623e55399b4eed0bfb5
Andy Dirnberger <andy.dirnberger@gmail.com>
2020-05-10 23:53:36 -0400

    Use the real gettext_lazy
    Since [Django][django] 2.0, `ugettext_lazy` has been an alias for
    `gettext_lazy`. By switching to the real implementation, the deprecation
    warnings will go away, making the output from the unit tests much easier
    to parse.
    
    [django]: https://www.djangoproject.com

--- src/users/forms.py
+++ src/users/forms.py
@@ -1,8 +1,9 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 
 class LoginForm(forms.Form):
     email = forms.EmailField(
+        # pyre-ignore[16]: This is fixed by https://github.com/facebook/pyre-check/pull/262.
         widget=forms.EmailInput(attrs={"placeholder": _("Email address")})
     )
